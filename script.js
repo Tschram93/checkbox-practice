08c93f3f456f3b4f98b1dfb92196ed7df277ebfb
TS93 <tschram93@yahoo.com>
2021-10-28 18:41:20 -0400

    shiftkey enabled and loop over checkboxes to check if selected

--- script.js
+++ script.js
@@ -3,12 +3,27 @@
 const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
 // console.log(checkboxes); //verifying it's selecting the checkboxes
 
+// Variables
+let lastChecked;
+
 // Event Listener
 // Loop over the checkboxes
 checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
 
 
 // Functions
-handleCheck(e) {
-    console.log(e);
+function handleCheck(e) {
+    // console.log(e);
+    let inBetween = false;
+    //Check if the shift key is pressed down and that they are checking the boxes (not un-checking)
+    if (e.shiftKey && this.checked) {
+        // Loop over checkboxes to see the first and last box that is 'checked'
+        checkboxes.forEach(checkbox => {
+            if (checkbox === this || checkbox === lastChecked) {
+                inBetween = !inBetween;
+            }
+        })
+    }
+
+    lastChecked = this;
 }
